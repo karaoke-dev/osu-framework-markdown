a263535112820f05d57315ca60046bba0d9962b2
為什麼 <andy840119@gmail.com>
2018-07-08 12:57:12 +0900

    add some testcase

--- osu.Framework.Markdown.Tests/Visual/TestCaseMarkdown.cs
+++ osu.Framework.Markdown.Tests/Visual/TestCaseMarkdown.cs
@@ -22,64 +22,99 @@ using osu.Framework.Allocation;
 
 namespace osu.Framework.Markdown.Tests.Visual
 {
-    [System.ComponentModel.Description("frame-based animations")]
+    [System.ComponentModel.Description("markdown reader")]
     public class TestCaseMarkdown : TestCase
     {
-        static void Error(string message)
-        {
-            Console.WriteLine(message);
-            Environment.Exit(1);
-        }
-
         public TestCaseMarkdown()
         {
-            var path = "https://github.com/lunet-io/scriban/blob/master/doc/language.md";
-            string markdown = null;
-            if (path.StartsWith("https:"))
-            {
-                Uri uri;
-                if (!Uri.TryCreate(path, UriKind.Absolute, out uri))
-                {
-                    Error($"Unable to parse Uri `{path}`");
-                    return;
-                }
-                // Special handling of github URL to access the raw content instead
-                if (uri.Host == "github.com")
-                {
-                    // https://github.com/lunet-io/scriban/blob/master/doc/language.md
-                    // https://raw.githubusercontent.com/lunet-io/scriban/master/doc/language.md
-                    var newPath = uri.AbsolutePath;
-                    var paths = new List<string>(newPath.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries));
-                    if (paths.Count < 5 || paths[2] != "blob")
-                    {
-                        Error($"Invalid github.com URL `{path}`");
-                        return;
-                    }
-                    paths.RemoveAt(2); // remove blob
-                    uri = new Uri($"https://raw.githubusercontent.com/{(string.Join("/", paths))}");
-                }
-
-                var httpClient = new HttpClient();
-                markdown = httpClient.GetStringAsync(uri).ConfigureAwait(false).GetAwaiter().GetResult();
-            }
-            else
-            {
-                markdown = File.ReadAllText(path);
-            }
-
-            var container = new MarkdownScrollContainer()
+            MarkdownScrollContainer markdownContainer;
+            Add(markdownContainer = new MarkdownScrollContainer()
             {
                 RelativeSizeAxes = Axes.Both,
-            };
-            
-            
-            //markdown= @"![Drag Racing](https://www.wonderplugin.com/videos/demo-image0.jpg)";
-             
+            });
 
-            container.MarkdownText = markdown;
-            
+            AddStep("Markdown Heading", () =>
+            {
+                markdownContainer.MarkdownText = @"# Header 1
+                ## Header 2
+                ### Header 3
+                #### Header 4
+                ##### Header 5";
+            });
 
-            Add(container);
+            AddStep("Markdown Seperator", () =>
+            {
+                markdownContainer.MarkdownText = @"# Language
+
+";
+            });
+
+            AddStep("Markdown Heading", () =>
+            {
+                markdownContainer.MarkdownText = @"- [1. Blocks](#1-blocks)
+  - [1.1 Code block](#11-code-block)
+  - [1.2 Text block](#12-text-block)
+  - [1.3 Escape block](#13-escape-block)
+  - [1.4 Whitespace control](#14-whitespace-control)
+- [2 Comments](#2-comments)
+- [3 Literals](#3-literals)
+  - [3.1 Strings](#31-strings)
+  - [3.2 Numbers](#32-numbers)
+  - [3.3 Boolean](#33-boolean)
+  - [3.4 null](#34-null)";
+            });
+
+            AddStep("Markdown Quote", () =>
+            {
+                markdownContainer.MarkdownText = @"> **input**";
+            });
+
+            AddStep("Markdown Fenced Code", () =>
+            {
+                markdownContainer.MarkdownText = @"```scriban-html
+{{
+  x = ""5""   # This assignment will not output anything
+  x         # This expression will print 5
+  x + 1     # This expression will print 6
+}}
+```";
+            });
+
+            AddStep("Markdown Paragraph 1", () =>
+            {
+                markdownContainer.MarkdownText = @"A text enclosed by `{{` and `}}` is a scriban **code block** that will be evaluated by the scriban templating engine.";
+            });
+
+            AddStep("Markdown Paragraph 2", () =>
+            {
+                markdownContainer.MarkdownText =
+                    @"The greedy mode using the character - (e.g {{- or -}}), removes any whitespace, including newlines Examples with the variable name = ""foo"":";
+            });
+
+            AddStep("MarkdownImage", () =>
+            {
+                markdownContainer.MarkdownText = @"![Drag Racing](https://www.wonderplugin.com/videos/demo-image0.jpg)
+![Drag Racing](https://www.wonderplugin.com/videos/demo-image0.jpg)
+![Drag Racing](https://www.wonderplugin.com/videos/demo-image0.jpg)
+![Drag Racing](https://www.wonderplugin.com/videos/demo-image0.jpg)
+![Drag Racing](https://www.wonderplugin.com/videos/demo-image0.jpg)";
+            });
+
+            AddStep("MarkdownFromInternet", () =>
+            {
+                try
+                {
+                    //test readme in https://github.com/lunet-io/scriban/blob/master/doc/language.md#92-if-expression-else-else-if-expression
+                    var url = "https://raw.githubusercontent.com/lunet-io/scriban/master/doc/language.md";
+                    var httpClient = new HttpClient();
+                    markdownContainer.MarkdownText = httpClient.GetStringAsync(url).ConfigureAwait(false).GetAwaiter().GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            });
         }
     }
 
@@ -164,7 +199,7 @@ namespace osu.Framework.Markdown.Tests.Visual
         {
             if (markdownObject is HeadingBlock headingBlock)
             {
-                container.Add(new MarkdownHeadingBlock(headingBlock));
+                container.Add(new MarkdownHeading(headingBlock));
             }
             else if (markdownObject is LiteralInline literalInline)
             {
@@ -295,11 +330,11 @@ namespace osu.Framework.Markdown.Tests.Visual
     /// ###Heading3
     /// ###3Heading4
     /// </summary>
-    internal class MarkdownHeadingBlock : Container
+    internal class MarkdownHeading : Container
     {
         private readonly MarkdownTextFlowContainer textFlowContainer;
 
-        public MarkdownHeadingBlock(HeadingBlock headingBlock)
+        public MarkdownHeading(HeadingBlock headingBlock)
         {
             AutoSizeAxes = Axes.Y;
             RelativeSizeAxes = Axes.X;
